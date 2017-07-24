d9ce36ac5108908d6ea7364b5271550fbb1f87f4
mimi89999 <michel@lebihan.pl>
2017-07-24 11:24:14 +0200

    Remove GMS dependency

--- Flym/src/main/java/net/fred/feedex/activity/BaseActivity.java
+++ Flym/src/main/java/net/fred/feedex/activity/BaseActivity.java
@@ -27,11 +27,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.security.ProviderInstaller;
-
 import net.fred.feedex.Constants;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -45,7 +40,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        updateAndroidSecurityProvider(this);
         mDecorView = getWindow().getDecorView();
 
         // For immersive mode
@@ -179,20 +173,6 @@ public abstract class BaseActivity extends AppCompatActivity {
             if (mOnFullScreenListener != null) {
                 mOnFullScreenListener.onFullScreenDisabled();
             }
-        }
-    }
-
-    // Fix to allow Android < 5.0 to access HTTPS feeds
-    // For more info see https://stackoverflow.com/a/36892715
-    private void updateAndroidSecurityProvider(Activity callingActivity) {
-        try {
-            ProviderInstaller.installIfNeeded(this);
-        } catch (GooglePlayServicesRepairableException e) {
-            // Thrown when Google Play Services is not installed, up-to-date, or enabled
-            // Show dialog to allow users to install, update, or otherwise enable Google Play services.
-            GooglePlayServicesUtil.getErrorDialog(e.getConnectionStatusCode(), callingActivity, 0);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Log.e("SecurityException", "Google Play Services not available.");
         }
     }
 
--- Flym/src/main/java/net/fred/feedex/activity/HomeActivity.java
+++ Flym/src/main/java/net/fred/feedex/activity/HomeActivity.java
@@ -50,8 +50,8 @@ import net.fred.feedex.fragment.EntriesListFragment;
 import net.fred.feedex.parser.OPML;
 import net.fred.feedex.provider.FeedData.EntryColumns;
 import net.fred.feedex.provider.FeedData.FeedColumns;
-import net.fred.feedex.service.AutoRefreshService;
 import net.fred.feedex.service.FetcherService;
+import net.fred.feedex.service.RefreshService;
 import net.fred.feedex.utils.PrefUtils;
 import net.fred.feedex.utils.UiUtils;
 
@@ -133,8 +133,12 @@ public class HomeActivity extends BaseActivity implements LoaderManager.LoaderCa
 
         getLoaderManager().initLoader(LOADER_ID, null, this);
 
-        AutoRefreshService.initAutoRefresh(this);
-
+        if (PrefUtils.getBoolean(PrefUtils.REFRESH_ENABLED, true)) {
+            // starts the service independent to this activity
+            startService(new Intent(this, RefreshService.class));
+        } else {
+            stopService(new Intent(this, RefreshService.class));
+        }
         if (PrefUtils.getBoolean(PrefUtils.REFRESH_ON_OPEN_ENABLED, false)) {
             if (!PrefUtils.getBoolean(PrefUtils.IS_REFRESHING, false)) {
                 startService(new Intent(HomeActivity.this, FetcherService.class).setAction(FetcherService.ACTION_REFRESH_FEEDS));
